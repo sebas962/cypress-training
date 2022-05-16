592629be335564d064ea6860563853cf9c0307d6
Sebastián Gómez Graciano <sebastian.gomezg@perficient.com>
2022-05-16 11:20:32 -0500

    Delivery for review of tenth point it was modified menu content, index and were added dress classes

---
+++ cypress/integration/dresses-list.spec.ts
@@ -0,0 +1,32 @@
+import { MenuContentPage, DressesListPage } from "../page/index";
+
+describe("the user navigates to the dresses page should", () => {
+  let menuContentPage: MenuContentPage;
+  let dressesListPage: DressesListPage;
+  let allDresses: string[];
+
+  before(() => {
+    menuContentPage = new MenuContentPage();
+    dressesListPage = new DressesListPage();
+    allDresses = [
+      "Printed Dress",
+      "Printed Dress",
+      "Printed Summer Dress",
+      "Printed Summer Dress",
+      "Printed Chiffon Dress",
+    ];
+  });
+
+  it("show the available dresses", () => {
+    //Arrange
+    menuContentPage.visitMenuContentPage();
+    menuContentPage.goDressesMenu();
+
+    //Act
+    dressesListPage.getDressProducts();
+
+    //Assert
+    dressesListPage.validateItemsNumber(allDresses.length);
+    dressesListPage.validateItemsNames(allDresses);
+  });
+});
